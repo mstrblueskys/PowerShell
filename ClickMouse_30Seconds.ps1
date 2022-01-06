69c178e3e9f73d853b8ab2513942492ad8465c18
Matt Peterson <matt.mjpete@gmail.com>
2022-01-06 16:00:38 -0600

    Create ClickMouse_30Seconds.ps1

---
+++ ClickMouse_30Seconds.ps1
@@ -0,0 +1,25 @@
+$ClickGo=1 
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")  
+
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms") 
+
+$signature=@' 
+
+[DllImport("user32.dll",CharSet=CharSet.Auto,CallingConvention=CallingConvention.StdCall)] 
+
+public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo); 
+
+'@  
+
+$SendMouseClick = Add-Type -memberDefinition $signature -name "Win32MouseEventNew" -namespace Win32Functions -passThru 
+
+While ($ClickGo = 1){ 
+
+    $SendMouseClick::mouse_event(0x00000002, 0, 0, 0, 0); 
+
+    $SendMouseClick::mouse_event(0x00000004, 0, 0, 0, 0); 
+
+    Start-Sleep -Seconds 3 
+
+} 
